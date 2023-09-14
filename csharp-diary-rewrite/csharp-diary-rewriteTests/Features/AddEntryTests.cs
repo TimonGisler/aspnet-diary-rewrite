d1adbf4b6b9785b0b3cc9be670e5bb937b5447d9
Your Name <timongisler@icloud.com>
2023-09-14 14:50:05 +0200

    wrote first test

---
+++ csharp-diary-rewrite/csharp-diary-rewriteTests/Features/AddEntryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace csharp_diary_rewrite.Features.Tests
+{
+    [TestClass()]
+    public class AddEntryTests
+    {
+        [TestMethod()]
+        public void SaveEntryTest()
+        {
+            var webFactory = new WebApplicationFactory<Program>();
+            var httpClient = webFactory.CreateClient();
+            
+            var response = httpClient.GetAsync("/api/hello").Result;
+            
+            Assert.AreEqual("HELLO", response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
