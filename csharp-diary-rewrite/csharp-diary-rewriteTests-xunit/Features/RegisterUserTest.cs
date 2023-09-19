e31bd9f9eab361590f6d3c84739385a79e7bdc29
Your Name <timongisler@icloud.com>
2023-09-19 21:05:09 +0200

    added an useful wrapper

--- csharp-diary-rewrite/csharp-diary-rewriteTests-xunit/Features/RegisterUserTest.cs
+++ csharp-diary-rewrite/csharp-diary-rewriteTests-xunit/Features/RegisterUserTest.cs
@@ -1,33 +1,30 @@
-﻿using System.Net.Http.Json;
-using csharp_diary_rewrite;
-using csharp_diary_rewrite.Features;
+﻿using csharp_diary_rewrite.Features;
 using csharp_diary_rewriteTests_xunit.Helpers;
-using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace csharp_diary_rewriteTests_xunit.Features;
 
-public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+public class RegisterUserTest : IClassFixture<DiaryApplicationWrapper>
 {
-    public RegisterUserTest(CustomWebApplicationFactory customWebApplicationFactory)
+    private readonly DiaryApplicationWrapper _diaryApplicationWrapper;
+    
+    public RegisterUserTest(DiaryApplicationWrapper diaryApplicationWrapper)
     {
-        this.customWebApplicationFactory = customWebApplicationFactory;
+        this._diaryApplicationWrapper = diaryApplicationWrapper;
     }
     
-    CustomWebApplicationFactory customWebApplicationFactory;
     
     [Fact]
     public void register_user_saves_user_in_database()
     {
-        var httpClient = customWebApplicationFactory.CreateClient();
-        var mail = "test@test.com";
-        var password = "test123";
+        const string mail = "test@test.com";
+        const string password = "test123";
         var registerUserCommand = new RegisterUserCommand(mail, password);
-        var diaryDbContext = customWebApplicationFactory.diaryDbContext;
-        var response = httpClient.PostAsJsonAsync("/api/register", registerUserCommand).Result;
+        
+        var response = _diaryApplicationWrapper.RegisterUser(registerUserCommand);
         
         response.EnsureSuccessStatusCode(); //test if the status code is a success code (200-299)
-        var user = diaryDbContext.Users.FirstOrDefault(u => u.Email == mail);
+        var user = _diaryApplicationWrapper.GetDbContext().Users.FirstOrDefault(u => u.Email == mail);
         Assert.NotNull(user); //todo tgis, make this test non modifying https://learn.microsoft.com/en-us/ef/core/testing/testing-with-the-database#tests-which-modify-data
     }
 }
---
+++ csharp-diary-rewrite/csharp-diary-rewriteTests-xunit/Helpers/DiaryApplicationWrapper.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Json;
+using csharp_diary_rewrite.Features;
+using csharp_diary_rewrite.Model;
+
+namespace csharp_diary_rewriteTests_xunit.Helpers;
+
+/**
+ * Provides a convenient api to interact with my application.
+ *
+ * Tests can use this class to make the correct api calls etc.
+ */
+public class DiaryApplicationWrapper
+{
+    private readonly HttpClient _httpClient;
+    private readonly DiaryDbContext _diaryDbContext;
+
+
+    public DiaryApplicationWrapper()
+    {
+        var customWebApplicationFactory = new CustomWebApplicationFactory();
+        _httpClient = customWebApplicationFactory.CreateClient();
+        _diaryDbContext = customWebApplicationFactory.diaryDbContext;
+    }
+
+    //register user
+    public HttpResponseMessage RegisterUser(RegisterUserCommand registerUserCommand)
+    {
+        return _httpClient.PostAsJsonAsync("/api/register", registerUserCommand).Result;
+    }
+
+
+    //get dbContext
+    public DiaryDbContext GetDbContext()
+    {
+        return _diaryDbContext;
+    }
+}
